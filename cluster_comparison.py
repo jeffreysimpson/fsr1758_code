9e6a2dba590845be8db69b2e941732e9e97e0351
Jeffrey Simpson <jeffrey.simpson@unsw.edu.au>
2019-01-28 15:38:21 +1100

    lots of style fixes

--- cluster_comparison.py
+++ cluster_comparison.py
@@ -25,11 +25,14 @@ __status__ = "Development"
 
 
 def compute_orbit(icrs, dt=-0.1*u.Myr, n_steps=50000):
+    """Change to Galactocentric frame and then integrate orbit."""
     c_icrs = icrs.transform_to(gc_frame).cartesian
     object_phase_space = gd.PhaseSpacePosition(
         pos=c_icrs.xyz,
         vel=c_icrs.differentials['s'].d_xyz)
-    return gp.Hamiltonian(pot).integrate_orbit(object_phase_space, dt=dt, n_steps=n_steps)
+    return gp.Hamiltonian(pot).integrate_orbit(object_phase_space,
+                                               dt=dt, n_steps=n_steps)
+
 
 pot = gp.MilkyWayPotential()
 v_sun = coord.CartesianDifferential([11, 248, 7.25]*u.km/u.s)
@@ -39,9 +42,10 @@ gc_frame = coord.Galactocentric(galcen_distance=8.2*u.kpc,
 
 clusters = pd.read_csv("../data/vasilev_catalogue.csv")
 
-clusters = clusters[~np.in1d(clusters.name,['E1',"Pal3","Pal4","Crater","Eridanus","NGC6121"])] #excluded due to large errors in PM
+# Exclude due to large errors in PM
+clusters = clusters[~np.in1d(clusters.name, ['E1', "Pal3", "Pal4", "Crater",
+                                             "Eridanus", "NGC6121"])]
 
-# clusters = clusters[np.in1d(clusters.name,['Crater'])]
 clusters_icrs = coord.ICRS(ra=clusters.ra.values * u.deg,
                            dec=clusters.dec.values * u.deg,
                            distance=clusters.dist.values * u.kpc,
@@ -63,15 +67,19 @@ cluster_apo = cluster_orbit.apocenter(approximate=True)
 cluster_e = cluster_orbit.eccentricity(approximate=True)
 cluster_r = cluster_orbit.spherical.distance[0]
 
-from matplotlib.ticker import ScalarFormatter
-
-annotate_pos = {"lower_left": dict(horizontalalignment='right', verticalalignment='top',
-                         xytext=(-1.1, -1.1), textcoords='offset pixels'),
-                    "lower_right": dict(verticalalignment='top',
-                         xytext=(1.1, 1.1), textcoords='offset pixels'),
-                    "upper_left": dict(horizontalalignment='right', verticalalignment='bottom',
-                         xytext=(1.1, 1.1), textcoords='offset pixels'),
-                    "upper_right": dict(xytext=(1.1, 1.1), textcoords='offset pixels')}
+annotate_pos = {"lower_left": dict(horizontalalignment='right',
+                                   verticalalignment='top',
+                                   xytext=(-1.1, -1.1),
+                                   textcoords='offset pixels'),
+                "lower_right": dict(verticalalignment='top',
+                                    xytext=(1.1, 1.1),
+                                    textcoords='offset pixels'),
+                "upper_left": dict(horizontalalignment='right',
+                                   verticalalignment='bottom',
+                                   xytext=(1.1, 1.1),
+                                   textcoords='offset pixels'),
+                "upper_right": dict(xytext=(1.1, 1.1),
+                                    textcoords='offset pixels')}
 
 cluster_name_dict = {"Terzan5": ["Terzan 5", annotate_pos['upper_right']],
                      "NGC5139": ["ω Cen", annotate_pos['lower_right']],
@@ -93,18 +101,22 @@ cluster_name_dict = {"Terzan5": ["Terzan 5", annotate_pos['upper_right']],
                      "NGC4833": ["NGC4833", annotate_pos['upper_left']],
                      "NGC5286": ["NGC5286", annotate_pos['upper_left']],
                      "NGC6205": ["NGC6205", annotate_pos['upper_left']],
-                     "NGC4833": ["NGC4833", annotate_pos['upper_left']],
                      "NGC6779": ["NGC6779", annotate_pos['upper_left']]}
 
-def plot_orbits(axes, peri, apo, e, Lz, r, s=1, marker='.', INTEREST = False, name=None):
+
+def plot_orbits(axes, peri, apo, e, Lz, r, s=1, marker='.',
+                INTEREST=False, name=None):
+    """Plot the orbits in various spaces."""
     axes[0].scatter(peri, apo, s=s, marker=marker)
     axes[2].scatter(e, apo, s=s, marker=marker)
     axes[3].scatter(Lz, apo, s=s, marker=marker)
     axes[1].scatter(r, apo, s=s, marker=marker)
     if INTEREST:
-        axes[0].annotate(cluster_name_dict[name][0], (peri.value, apo.value), fontsize=4,
-                     bbox=dict(boxstyle='square', fc='w', ec='none',alpha=0.3, pad=-0.2),
-                     **cluster_name_dict[name][1])
+        axes[0].annotate(cluster_name_dict[name][0], (peri.value, apo.value),
+                         fontsize=4, bbox=dict(boxstyle='square',
+                                               fc='w', ec='none', alpha=0.3,
+                                               pad=-0.2),
+                         **cluster_name_dict[name][1])
 
 
 fig, axes = plt.subplots(1, 4, figsize=(3.32*2, 2.0), sharey=True)
@@ -112,27 +124,28 @@ fig, axes = plt.subplots(1, 4, figsize=(3.32*2, 2.0), sharey=True)
 panel_labels = ['(a)', '(b)', '(c)', '(d)', '(e)', '(f)']
 for axes_count, ax in enumerate(axes.flatten()):
     ax.annotate(panel_labels[axes_count], (0.10, 0.90),
-            xycoords='axes fraction')
+                xycoords='axes fraction')
 
 plot_orbits(axes,
             cluster_peri, cluster_apo, cluster_e,
-            cluster_orbit.angular_momentum()[2][0,:].to(u.kpc*u.km/u.s), cluster_r)
+            cluster_orbit.angular_momentum()[2][0, :].to(u.kpc*u.km/u.s),
+            cluster_r)
 
 
 for ax in axes[0:2]:
-    ax.plot([0,200],[0,200],color='k',lw=0.5)
-    ax.axvspan(xmin=0,xmax=3,alpha=0.1,color='C1',lw=0.)
+    ax.plot([0, 200], [0, 200], color='k', lw=0.5)
+    ax.axvspan(xmin=0, xmax=3, alpha=0.1, color='C1', lw=0.)
 
 # for cluster_interest in []:
-for cluster_interest in ["Terzan5","NGC5139","NGC6715","NGC6656",
-                         "NGC7089","Djorg1","Terzan10","IC4499","NGC2419","NGC3201"]:
-
-# for cluster_interest in ['NGC362','NGC1851','NGC1904','NGC2298','NGC4833','NGC5139',
-#                          'NGC5286','NGC6205','NGC6341','NGC6779']:
+for cluster_interest in ["Terzan5", "NGC5139", "NGC6715", "NGC6656", "NGC7089",
+                         "Djorg1", "Terzan10", "IC4499", "NGC2419", "NGC3201"]:
     cluster_idx = np.in1d(clusters.name, [cluster_interest])
     plot_orbits(axes,
-                cluster_peri[cluster_idx], cluster_apo[cluster_idx], cluster_e[cluster_idx],
-                cluster_orbit.angular_momentum()[2][0,:].to(u.kpc*u.km/u.s)[cluster_idx], cluster_r[cluster_idx],
+                cluster_peri[cluster_idx],
+                cluster_apo[cluster_idx],
+                cluster_e[cluster_idx],
+                cluster_orbit.angular_momentum()[2][0,:].to(u.kpc*u.km/u.s)[cluster_idx],
+                cluster_r[cluster_idx],
                 s=3, marker='s', INTEREST=True, name=cluster_interest)
 
 plot_orbits(axes,
@@ -163,8 +176,7 @@ axes[0].set_xlim(0.1, 30)
 axes[0].set_ylim(0.8, 110)
 axes[1].set_xlim(0.3, 130)
 axes[2].set_xlim(0, 1)
-# axes[3].set_xlim(-4500, 3500)
 
 fig.align_labels()
-plt.savefig("../fsr1758_paper/figures/orbit_comparison_new.pdf", bbox_inches='tight')
-plt.show()
+plt.savefig("../fsr1758_paper/figures/orbit_comparison_new.pdf",
+            bbox_inches='tight')
