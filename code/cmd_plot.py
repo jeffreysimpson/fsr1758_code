d6409e16c31c9b40f5171515c7131e3a980660f2
Jeffrey Simpson <jeffrey.simpson@unsw.edu.au>
2019-06-12 13:53:50 +1000

    Refactor the code to use the common_code functionality
    Also rewrote how the idx are created which makes to easier to work out 
    what each is doing.

--- code/cmd_plot.py
+++ code/cmd_plot.py
@@ -5,36 +5,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from astropy import units as u
-from astropy.coordinates import SkyCoord
-from astropy.io import fits
+import common_code
 
 __author__ = "Jeffrey Simpson"
 __copyright__ = "Copyright 2019, Jeffrey Simpson"
 __credits__ = ["Jeffrey Simpson"]
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Jeffrey Simpson"
 __email__ = "jeffrey.simpson@unsw.edu.au"
 __status__ = "Development"
 
-sns.set_context("paper", font_scale=0.8)
 
-gaia_1p5deg = fits.open("../data/gaia_fsr1758.fits")
-fsr1758 = gaia_1p5deg[1].data
-c = SkyCoord(ra=fsr1758['ra']*u.degree,
-             dec=fsr1758['dec']*u.degree, frame='icrs')
-cluster_centre = SkyCoord(ra=262.806*u.degree,
-                          dec=-39.822*u.degree, frame='icrs')
-cluster_pos_idx = c.separation(cluster_centre) < 0.2*u.deg
-cluster_pm_idx = np.sqrt((fsr1758['pmra']--2.85)**2 +
-                         (fsr1758['pmdec']-2.55)**2) < 1.2
-large_pm_idx = (c.separation(cluster_centre) < 35*u.arcmin)
-radial_velocity_members_idx = fsr1758['radial_velocity'] > 200
+(fsr1758,
+ cluster_pos_idx,
+ cluster_pm_idx,
+ good_photom_idx,
+ good_astrom_idx) = common_code.open_file("../data/gaia_fsr1758.fits")
+fast_enough_rv_idx = fsr1758['radial_velocity'] > 200
+good_rv_idx = fsr1758['rv_nb_transits'] >= 5
 has_rv_idx = ~np.isnan(fsr1758['radial_velocity'])
 likely_cluster_idx = cluster_pos_idx & cluster_pm_idx
-# rrlyraeab_idx = fsr1758['Mode'] == "RRab"
-# rrlyraeac_idx = fsr1758['Mode'] == "RRc"
+
+# The field stars and cluster stars without radial velocities
+field_stars_idx = ~cluster_pos_idx & ~has_rv_idx
+cluster_stars_idx = cluster_pos_idx & ~has_rv_idx
+
+# The cluster stars with RVs
+cluster_star_rv_idx = cluster_pos_idx & has_rv_idx & good_rv_idx
+cluster_stars_right_rv_idx = cluster_star_rv_idx & fast_enough_rv_idx
+cluster_stars_wrong_rv_idx = cluster_star_rv_idx & ~fast_enough_rv_idx
+
+# The field stars with RVs
+field_star_rv_idx = ~cluster_pos_idx & has_rv_idx & good_rv_idx
+field_stars_right_rv_idx = field_star_rv_idx & fast_enough_rv_idx
+field_stars_wrong_rv_idx = field_star_rv_idx & ~fast_enough_rv_idx
+
+# Require for all that they have good astrometry
+idx_list = [idx & cluster_pm_idx & good_astrom_idx for idx in [
+    field_stars_idx, cluster_stars_idx,
+    cluster_stars_right_rv_idx, cluster_stars_wrong_rv_idx,
+    field_stars_right_rv_idx, field_stars_wrong_rv_idx]]
 
 
 panel_labels = ['(a)', '(b)', '(c)', '(d)', '(e)', '(f)']
@@ -54,17 +65,6 @@ axes_labels = [['RA (deg)', 'Dec (deg)'],
                ['Angular distance (deg)', r'$v_r$ (km s$^{-1}$'],
                [r'$\varpi$ (mas)', r'$G$']]
 
-idx_list = [cluster_pm_idx & ~cluster_pos_idx & ~has_rv_idx,
-            cluster_pm_idx & cluster_pos_idx & ~has_rv_idx,
-            cluster_pm_idx & cluster_pos_idx & radial_velocity_members_idx,
-            cluster_pm_idx & cluster_pos_idx & has_rv_idx & ~radial_velocity_members_idx,
-            cluster_pm_idx & ~cluster_pos_idx & radial_velocity_members_idx,
-            cluster_pm_idx & ~cluster_pos_idx & has_rv_idx & ~radial_velocity_members_idx,
-            # cluster_pm_idx & cluster_pos_idx & rrlyraeab_idx,
-            # cluster_pm_idx & ~cluster_pos_idx & rrlyraeab_idx,
-            # cluster_pm_idx & cluster_pos_idx & rrlyraeac_idx,
-            # cluster_pm_idx & ~cluster_pos_idx & rrlyraeac_idx]
-            ]
 
 plot_kwargs = [dict(alpha=0.5, s=2, c='#4daf4a', lw=0),
                dict(alpha=0.8, s=4, c='#984ea3', lw=0),
@@ -73,13 +73,7 @@ plot_kwargs = [dict(alpha=0.5, s=2, c='#4daf4a', lw=0),
                     facecolor='None', alpha=1., s=30, lw=0.4),
                dict(marker='*', alpha=1., s=60, c='k', lw=0),
                dict(marker='*', edgecolor='k',
-                    facecolor='None', alpha=1., s=30, lw=0.4),
-               # dict(marker='s', alpha=1., s=10, c='C0', lw=0),
-               # dict(marker='s', edgecolor='C0',
-               #      facecolor='None', alpha=1., s=10, lw=0.4),
-               # dict(marker='<', alpha=1., s=10, c='k', lw=0),
-               # dict(marker='<', edgecolor='k',
-               #      facecolor='None', alpha=1., s=10, lw=0.4)
+                    facecolor='None', alpha=1., s=30, lw=0.4)
                ]
 
 ra = 262.806
@@ -92,6 +86,7 @@ plot_limits = [[[ra-box, ra+box], [dec-(box-0.25), dec+(box-0.25)]],
                [[0.0, 1.1], [-160, 250]],
                [[-2, 2], [20.5, 10]]]
 
+sns.set_context("paper", font_scale=0.8)
 fig, axes = plt.subplots(ncols=3, nrows=2, figsize=(3.32*2, 4.5),
                          constrained_layout=True)
 
@@ -104,7 +99,8 @@ for axes_count, ax in enumerate(axes.flatten()):
     ax.set_ylim(plot_limits[axes_count][1])
     for idx_count, idx in enumerate(idx_list):
         if axes_count == 2:
-            idx = idx & good_photom_idx(gaia_1p5deg, FITS=True)
+            # Require that the CMD only has stars with good photometry.
+            idx = idx & good_photom_idx
         ax.scatter(fsr1758[xy_values[axes_count][0]][idx],
                    fsr1758[xy_values[axes_count][1]][idx],
                    **plot_kwargs[idx_count])
